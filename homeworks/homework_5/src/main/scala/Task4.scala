e4aaf2c817b12aaa56469595d0095382798a4b28
karina <akhmatovakar@yandex.ru>
2025-11-12 23:35:07 +0500

    homework_5

--- homeworks/homework_5/src/main/scala/Task4.scala
+++ homeworks/homework_5/src/main/scala/Task4.scala
@@ -16,10 +16,17 @@ object Task4 extends App {
     def pure[A](value: A): F[E, A]
     def flatMap[A, B](fa: F[E, A])(f: A => F[E, B]): F[E, B]
 
-    def map[A, B](fa: F[E, A])(f: A => B): F[E, B] = ???
+    def map[A, B](fa: F[E, A])(f: A => B): F[E, B] =
+      flatMap(fa)(a => pure(f(a)))
 
-    def raiseError[A](fa: F[E, A])(error: => E):  F[E, A]
-    def handleError[A](fa: F[E, A])(handle: E => A): F[E, A]
+    def raiseError[A](fa: F[E, A])(error: => E): F[E, A] =
+      flatMap(fa)(_ => pure(error).asInstanceOf[F[E, A]])
+
+    def handleError[A](fa: F[E, A])(handle: E => A): F[E, A] =
+      fa match {
+        case EIO(Left(e: E)) => pure(handle(e))
+        case _ => fa
+      }
   }
 
   case class EIO[+E, +A](value: Either[E, A])
@@ -28,9 +35,21 @@ object Task4 extends App {
 
     def error[E, A](error: E): EIO[E, A] = EIO[E, A](Left(error))
 
-    def possibleError[A](f: => A): EIO[Throwable, A] = ???
+    def possibleError[A](f: => A): EIO[Throwable, A] =
+      EIO(Try(f).toEither)
 
-    implicit def monad[E]: MonadError[EIO, E] = ???
+    implicit def monad[E]: MonadError[EIO, E] = new MonadError[EIO, E] {
+      def pure[A](value: A): EIO[E, A] = EIO(value)
+
+      def flatMap[A, B](fa: EIO[E, A])(f: A => EIO[E, B]): EIO[E, B] =
+        fa.value match {
+          case Right(a) => f(a)
+          case Left(e) => EIO(Left(e))
+        }
+
+      override def raiseError[A](fa: EIO[E, A])(error: => E): EIO[E, A] =
+        EIO(Left(error))
+    }
   }
 
   object EIOSyntax {
