705cfdbd449b31ab08705cc73e5ea7f626e878b7
SergeiDagaev <pazni75052@gmail.com>
2025-12-08 17:58:56 +0300

    Update ResuourceTraining.scala

--- homeworks/homework_7/src/main/scala/ResuourceTraining.scala
+++ homeworks/homework_7/src/main/scala/ResuourceTraining.scala
@@ -4,18 +4,44 @@ import zio.{IO, Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 
 import java.io.{BufferedReader, BufferedWriter, FileReader, FileWriter}
 
-
-/**
- * Необходимо реализовать функции readData и writeData, записывающие и читающие данные в/из файла соответственно.
- * В реализации следует применять безопасное использование ресурсов ZIO.acquireReleaseWith
- */
-
-
 object ResuourceTraining extends ZIOAppDefault {
 
-  def readData(filePath: String): IO[Throwable, String] = ???
+  def readData(filePath: String): IO[Throwable, String] = {
+    ZIO.acquireReleaseWith(
+      // Acquire: открываем BufferedReader
+      ZIO.attempt(new BufferedReader(new FileReader(filePath)))
+    )(
+      // Release: гарантированно закрываем reader
+      reader => ZIO.succeed(reader.close()).ignore
+    ) { reader =>
+      // Use: читаем все содержимое файла
+      ZIO.attempt {
+        val sb = new StringBuilder
+        var line: String = null
 
-  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = ???
+        while ({ line = reader.readLine(); line != null }) {
+          if (sb.nonEmpty) sb.append(System.lineSeparator())
+          sb.append(line)
+        }
 
-  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed("Done")
+        sb.toString()
+      }
+    }
+  }
+
+  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = {
+    ZIO.acquireReleaseWith(
+      // Acquire: открываем BufferedWriter
+      ZIO.attempt(new BufferedWriter(new FileWriter(filePath, false)))
+    )(
+      // Release: гарантированно закрываем writer
+      writer => ZIO.succeed(writer.close()).ignore
+    ) { writer =>
+      // Use: записываем данные
+      ZIO.attempt(writer.write(data)).unit
+    }.catchAll(_ => ZIO.unit) // Игнорируем все ошибки
+  }
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
+    ZIO.succeed("Done")
 }
